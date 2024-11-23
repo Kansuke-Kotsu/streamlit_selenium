b205efe8c3bbb51e372ae5fd48e63d51582df466
Kansuke-Kotsu <85889110+Kansuke-Kotsu@users.noreply.github.com>
2024-11-23 14:16:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,8 @@ st.title("Search property")
 password = st.text_input("パスワード", type="password")
  
 if password == st.secrets["password"]:
-    st.success("アクセスが許可されました。")
-else:
-    st.error("アクセスが拒否されました。")
-# --- End Authentication ---
-
-input_1 = st.text_input("物件名・キーワードを入力してください。")
-options = [
+    input_1 = st.text_input("物件名・キーワードを入力してください。")
+    options = [
     "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
     "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
     "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県",
@@ -24,8 +19,12 @@ options = [
     "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県",
     "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県",
     "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県"
-]
-input_2 = st.selectbox("都道府県名を選択してください。", options)
+    ]
+    input_2 = st.selectbox("都道府県名を選択してください。", options)
+else:
+    st.error("アクセスが拒否されました。")
+# --- End Authentication ---
+
 
 def invoke_lambda(api_gateway_url, payload, site_name):
     headers = {'Content-Type': 'application/json'}
