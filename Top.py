c82a314af8ce5fb1ebddc3c7a2c09093209d06b4
Kansuke-Kotsu <85889110+Kansuke-Kotsu@users.noreply.github.com>
2025-02-01 21:28:06 +0900

    Update Top.py

--- Top.py
+++ Top.py
@@ -7,39 +7,39 @@ with open("style.css", encoding="utf-8") as f:
 
 
 # --- Authentication ---
-#password = st.text_input("パスワード", type="password")
-#if password == st.secrets["password"]:
-# カラムの配置
-# 1行目の3つのカラム
-col1, col2 = st.columns(2)
+password = st.text_input("パスワード", type="password")
+if password == st.secrets["password"]:
+    # カラムの配置
+    # 1行目の3つのカラム
+    col1, col2 = st.columns(2)
 
-with col1:
-    st.markdown(
-        '''
-        <a href="/Search">
-            <div class="container">
-                <p>① 以下のサイトから検索</p>
-                <p>・レインズ</p>
-                <p>・エイブル</p>
-                <p>・東急</p>
-                <p>・宅都</p>
-            </div>
-        </a>
-        ''',
-        unsafe_allow_html=True
-    )
+    with col1:
+        st.markdown(
+            '''
+            <a href="/Search">
+                <div class="container">
+                    <p>① 以下のサイトから検索</p>
+                    <p>・レインズ</p>
+                    <p>・エイブル</p>
+                    <p>・東急</p>
+                    <p>・宅都</p>
+                </div>
+            </a>
+            ''',
+            unsafe_allow_html=True
+        )
 
-with col2:
-    st.markdown(
-        '''
-        <a href="/REINS_detail">
-            <div class="container">
-                <p>② レインズから詳細な情報を含めて検索</p>
-                <p>・Adとか含め検索</p>
-            </div>
-        </a>
-        ''',
-        unsafe_allow_html=True
-    )
-#else:
-#    st.write("合言葉は？")
+    with col2:
+        st.markdown(
+            '''
+            <a href="/REINS_detail">
+                <div class="container">
+                    <p>② レインズから詳細な情報を含めて検索</p>
+                    <p>・Adとか含め検索</p>
+                </div>
+            </a>
+            ''',
+            unsafe_allow_html=True
+        )
+else:
+    st.write("合言葉は？")
